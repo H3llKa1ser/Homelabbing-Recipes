f075e35249584bbaaad2b810b820483a69c29301
H3ll_Ka1ser <98783688+H3llKa1ser@users.noreply.github.com>
2025-09-25 23:55:56 +0300

    Update main.tf

--- Terraform/Azure/Configs/main.tf
+++ Terraform/Azure/Configs/main.tf
@@ -9,7 +9,12 @@ terraform {
 
   required_version = ">= 1.1.0"
 }
-
+cloud {
+    organization = "<ORG_NAME>"
+    workspaces {
+      name = "learn-terraform-azure"
+    }
+  }
 provider "azurerm" {
   features {}
 }
